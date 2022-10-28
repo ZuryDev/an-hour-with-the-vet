9997640b36ed6a2e82e3199aa65aeb1f72ed1400
ZuryDev <zurydevs@gmail.com>
2022-10-28 08:59:08 -0300

    create component .form.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React, {Fragment} from 'react'
+import { Form } from './components/Form';
 
 function App() {
   return (
-    <h1>Medical Appointment Manager</h1>
+    <Fragment>
+      <h1>Medical Appointment Manager</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form/>
+          </div>
+          <div className="one-half column">
+            2
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
