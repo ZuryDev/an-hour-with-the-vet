a48b12b6423549ab2e4cb98630fdca5fc05e48e6
ZuryDev <zurydevs@gmail.com>
2022-10-28 17:59:23 -0300

    problems with localStorage and react hook, check later and fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React, {Fragment, useState} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import { Form } from './components/Form';
 import { Appointment } from './components/Appointment';
 
 function App() {
 
+  //citas en el local storage
+  /*let appointmentInit = JSON.parse(localStorage.getItem('appointments'));
+  if(!appointmentInit) {
+    appointmentInit = [];
+  } */
+
+  //array de citas
 const [appointments, saveAppointment] = useState([]);
 
+
+//useEfect para realizar ciertas operaciones cuando el state cambia
+/*useEffect(() => {
+  let appointmentInit = JSON.parse(localStorage.getItem('appointments'));
+
+  if(appointmentInit){
+    localStorage.setItem('appointments', JSON.stringify(appointments))
+  } else {
+    localStorage.setItem('appointments', JSON.stringify([]));
+  }
+}, [appointments]); */
+
+
+//funcion que tome las citas actuales y agregue la nueva 
 const createAppointment = appointment => {
   saveAppointment([
     ...appointments, 
     appointment
   ]);
 }
-
+//funcion que elimina una cita por su id 
 const deleteAppointment = id => {
   const newAppointment = appointments.filter(appointment => appointment.id !== id);
   saveAppointment(newAppointment)
 }
 
+//mensaje condicional
 const title = appointments.length === 0 ? 'no dating' : 'manage your appointments';
+
 
   return (
     <Fragment>
