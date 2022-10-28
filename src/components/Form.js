ad8b2287d57d77d0a2e6c8e840d6e2f84653c869
ZuryDev <zurydevs@gmail.com>
2022-10-28 09:49:09 -0300

    adding form fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 
 export const Form = () => {
   return (
-    <div>Form</div>
+    <Fragment>
+        <h2>create appointment</h2>
+
+        <form>
+            <label>Pet Name</label>
+            <input
+            type="text"
+            name="pet"
+            className="u-full-width"
+            placeholder="Pet Name"
+            />
+            <label>Pet owner name</label>
+            <input
+            type="text"
+            name="pet owner"
+            className="u-full-width"
+            placeholder="Pet owner name"
+            />
+            <label>Date</label>
+            <input
+            type="date"
+            name="date"
+            className="u-full-width"
+            />
+            <label>Hour</label>
+            <input
+            type="time"
+            name="hour"
+            className="u-full-width"
+            />
+             <label>Symptoms</label>
+            <textarea
+            className="u-full-width"
+            name="symptons"
+            ></textarea>
+
+            <button
+            type="submit"
+            className="u-full-width button-primary"
+            >save appointment</button>
+        </form>
+    </Fragment>
   )
 }
 
