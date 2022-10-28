096251b39eafea22cd25dd96b0d061b34fa1a338
ZuryDev <zurydevs@gmail.com>
2022-10-28 10:08:03 -0300

    creating the state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,19 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 
 export const Form = () => {
+
+  const [appointment, setAppointment] = useState({
+    pet: '',
+    owner: '',
+    date: '',
+    hour: '',
+    symptoms: ''
+  });
+
+  const updateStatus = () => {
+    console.log('escribiendo...');
+  }
+
   return (
     <Fragment>
         <h2>create appointment</h2>
@@ -12,30 +25,35 @@ export const Form = () => {
             name="pet"
             className="u-full-width"
             placeholder="Pet Name"
+            onChange={updateStatus}
             />
             <label>Pet owner name</label>
             <input
             type="text"
-            name="pet owner"
+            name="owner"
             className="u-full-width"
             placeholder="Pet owner name"
+            onChange={updateStatus}
             />
             <label>Date</label>
             <input
             type="date"
             name="date"
             className="u-full-width"
+            onChange={updateStatus}
             />
             <label>Hour</label>
             <input
             type="time"
             name="hour"
             className="u-full-width"
+            onChange={updateStatus}
             />
              <label>Symptoms</label>
             <textarea
             className="u-full-width"
             name="symptons"
+            onChange={updateStatus}
             ></textarea>
 
             <button
